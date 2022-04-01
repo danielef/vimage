c2358eea25e03b78ee9e3f2e0ccc51c6dd891689
Daniel Estevez <daniel.ef@gmail.com>
2022-04-01 01:41:04 -0400

    Fix identation

--- ndd.py
+++ ndd.py
@@ -3,12 +3,12 @@ import logging
 import numpy
 
 def binary_array_to_hex(arr):
-	"""
-	internal function to make a hex string out of a binary array.
-	"""
-	bit_string = ''.join(str(b) for b in 1 * arr.flatten())
-	width = int(numpy.ceil(len(bit_string)/4))
-	return '{:0>{width}x}'.format(int(bit_string, 2), width=width)
+    """
+    internal function to make a hex string out of a binary array.
+    """
+    bit_string = ''.join(str(b) for b in 1 * arr.flatten())
+    width = int(numpy.ceil(len(bit_string)/4))
+    return '{:0>{width}x}'.format(int(bit_string, 2), width=width)
 
 def dhash(image, hash_size=8):
     if hash_size < 2:
